3bdf482c1f1881b1104e45fb6ae1819b19305e6d
Leontien90 <leontienboere@hotmail.com>
2014-05-19 10:54:28 +0200

    Nokogiri stuff

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+require 'rubygems'
+require 'nokogiri'
+require 'open-uri'
